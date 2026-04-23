e9ed9fe01facfde9ce9f0d89a94005a2bfdc0361
Aldrin Suarez Jr. <aldrinextra000@gmail.com>
2026-04-23 16:09:08 +0800

    may changes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('items/', include('items.urls')),
-    path('claim/', include('claim.urls'))
+    path('item/', include('items.urls')),
+    path('claim/', include('claim.urls')),
+    path('notification/', include('notification.urls')),
 ]
